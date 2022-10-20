fa91e029e3ea6d26631761957c4649e56f1aa50a
kenjidoom <vitdevcool@protonmail.com>
2022-10-19 19:30:03 -0700

    Root permissions needed

--- monitor-mode.sh
+++ monitor-mode.sh
@@ -1,4 +1,10 @@
-echo "Ex: ./monitor-mode {INTERFACE}"
-airmon-ng check kill
-airmon-ng start $1
-airodump-ng $1
+if [ ! "`whoami`" = "root" ]
+then
+    echo "Please run script as root."
+    exit 1
+else
+	echo "Ex: ./monitor-mode {INTERFACE}"
+	airmon-ng check kill
+	airmon-ng start $1
+	airodump-ng $1
+fi
